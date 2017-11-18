63d23a062ac5ba71e09f8b6068e3a2cec12714f0
Amin Shah Gilani <amin@gilani.me>
2017-11-18 16:51:45 +0500

    Customize homepage

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+import harambe from './harambe.jpg';
+import './App.css';
+const ContainerExampleText = () => (
+  <Container textAlign={'left'} text>
+    <figure>
+      <img src={harambe} width="500px"/>
+      <figcaption>Harambe, 1999 - 2016</figcaption>
+    </figure>
+    <Header textAlign={'center'} as='h2'>This shouldn't be here</Header>
+    <p>
+      Hi<br />
+      This tribute to Harambe should be here. I don't know who to get in touch
+      with about this domain, but if you realize the important of talking to me, get
+      in touch.
+    </p>
+    <p>
+    Amin Shah Gilani (<a href="https://keybase.io/gilani">PGP Key</a>)
+    </p>
+  </Container>
+)
+
+
+class App extends Component {
+  render() {
+    return (
+      <ContainerExampleText />
+    );
+  }
+}
+
+export default App;
