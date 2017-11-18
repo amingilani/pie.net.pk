0721db9fff2e2d80de9d5fd500d24b8092d2225f
Amin Shah Gilani <amin@gilani.me>
2017-11-18 17:04:20 +0500

    Fix grammer

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,8 @@ const ContainerExampleText = () => (
     <Header textAlign={'center'} as='h2'>This shouldn't be here</Header>
     <p>
       Hi<br />
-      This tribute to Harambe should be here. I don't know who to get in touch
-      with about this domain, but if you realize the important of talking to me, get
-      in touch.
+      This tribute to Harambe should be here. I don't know who to contact
+      about this domain, but if it's you, get in touch.
     </p>
     <p>
     Amin Shah Gilani (<a href="https://keybase.io/gilani">PGP Key</a>)
